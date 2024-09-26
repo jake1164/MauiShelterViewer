57a46d8c582699e173e2a6658e085c53ffc2ea72
Jason Jackson <jake1164@hotmail.com>
2024-09-26 08:27:39 -0400

    Added vault service for processing vault info

---
+++ ShelterViewer/Services/VaultService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelterViewer.Services;
+
+public class VaultService
+{
+    public event PropertyChangingEventHandler? OnVaultChanged = null;
+    public void InitializeVault(string vaultJsonString)
+    {
+        VaultString = vaultJsonString;
+        NotifyPropertyChanged();
+    }
+    private string VaultString { get; set; } = String.Empty;
+
+    public string Name { get; set; }
+
+    public bool IsVaultEmpty()
+    {
+        return VaultString == String.Empty;
+    }
+
+    private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        if (OnVaultChanged == null)
+            return;
+
+        if(propertyName != "")
+        {
+            OnVaultChanged(this, new PropertyChangingEventArgs(propertyName));
+        }
+    }
+}
