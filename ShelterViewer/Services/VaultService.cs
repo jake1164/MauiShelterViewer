5be5ac4d51bb153135fdc81b47648b4a7489981c
Jason Jackson <jake1164@hotmail.com>
2024-09-26 18:19:03 -0400

    Populated menu and created default pages

--- ShelterViewer/Services/VaultService.cs
+++ ShelterViewer/Services/VaultService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -10,18 +11,53 @@ namespace ShelterViewer.Services;
 public class VaultService
 {
     public event PropertyChangingEventHandler? OnVaultChanged = null;
-    public void InitializeVault(string vaultJsonString)
-    {
-        VaultString = vaultJsonString;
-        NotifyPropertyChanged();
-    }
-    private string VaultString { get; set; } = String.Empty;
 
-    public string Name { get; set; }
+    private string _vaultString = String.Empty;
+
+    private dynamic? _vaultData = null;
+
+    public string Name 
+    { 
+        get 
+        { 
+            return _vaultData?.vault.VaultName ?? String.Empty; 
+        } 
+    }
+
+    public int DwellerCount
+    {
+        get
+        {
+            return _vaultData?.dwellers.dwellers.Count ?? 0;
+        }
+    }
+
+    public int RoomCount
+    {
+        get
+        {
+            return _vaultData?.vault.rooms.Count ?? 0;
+        }
+    }
 
     public bool IsVaultEmpty()
     {
-        return VaultString == String.Empty;
+        return _vaultString == String.Empty;
+    }
+    public void InitializeVault(string vaultJsonString)
+    {
+        try
+        {
+            _vaultString = vaultJsonString;
+            _vaultData = JsonConvert.DeserializeObject<dynamic>(_vaultString);
+
+            NotifyPropertyChanged();
+        } 
+        catch (Exception ex)
+        {
+            _vaultString = String.Empty;
+            Console.WriteLine("Unable to convert vault string to JSON Object: " + ex.Message);
+        }
     }
 
     private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
