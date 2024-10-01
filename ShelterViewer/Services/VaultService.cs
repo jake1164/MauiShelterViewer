92e83b70042396c433d7ebee99c48aa8c2363364
Jason Jackson <jake1164@hotmail.com>
2024-10-01 07:35:29 -0400

    Fixed parsing of rooms

--- ShelterViewer/Services/VaultService.cs
+++ ShelterViewer/Services/VaultService.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using ShelterViewer.Models; 
+using ShelterViewer.Models;
+using ShelterViewer.Utility;
 
 namespace ShelterViewer.Services;
 
@@ -62,7 +63,7 @@ public class VaultService
 
         try
         {
-            var settings = new JsonSerializerSettings();
+            var settings = new IntJsonConverter();
             _vaultString = vaultJsonString;
             _vaultData = JsonConvert.DeserializeObject<dynamic>(_vaultString, settings);
 
@@ -81,7 +82,7 @@ public class VaultService
 
     public List<Dweller> GetDwellers()
     {
-        var settings = new JsonSerializerSettings();
+        var settings = new IntJsonConverter();
         List<Dweller> dwellers = new();
         if (_vaultData == null)
             return new();
@@ -105,7 +106,7 @@ public class VaultService
 
     public List<Room> GetRooms()
     {
-        var settings = new JsonSerializerSettings();
+        var settings = new IntJsonConverter();
         List<Room> rooms = new();
         if (_vaultData == null)
             return new();
